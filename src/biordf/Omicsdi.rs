1ab51bb54125e2cae6e6747fc29743cb5a5962be
Sibbe Bakker <s.l.bakker@uu.nl>
2025-01-22 12:08:38 +0100

    basic api connection test

--- src/biordf/Omicsdi.rs
+++ src/biordf/Omicsdi.rs
@@ -7,44 +7,45 @@ pub mod DataSet {
 
     #[derive(Deserialize, Debug, Clone)]
     pub struct OmicsDiResponse {
-        count: u64,
+        pub count: u64,
         pub datasets: Vec<DataSet>,
-        facets: Vec<Facet>,
+        pub facets: Vec<Facet>,
     }
 
     #[derive(Deserialize, Debug, Clone)]
     pub struct DataSet {
         pub id: String,
-        source: String,
-        title: String,
-        description: Option<String>,
-        organisms: Vec<Organism>,
-        publicationDate: Option<String>,
-        omicsType: Vec<String>,
-        citationsCount: Option<u64>,
+        pub source: String,
+        pub title: String,
+        pub description: Option<String>,
+        pub organisms: Vec<Organism>,
+        pub publicationDate: Option<String>,
+        pub omicsType: Vec<String>,
+        pub citationsCount: Option<u64>,
     }
 
     #[derive(Deserialize, Debug, Clone)]
     pub struct Organism {
-        acc: Option<String>,
-        name: String,
+        pub acc: Option<String>,
+        pub name: String,
     }
 
     #[derive(Deserialize, Debug, Clone)]
     pub struct Facet {
-        id: String,
-        label: String,
-        total: u64,
-        facetValues: Vec<FacetValue>,
+        pub id: String,
+        pub label: String,
+        pub total: u64,
+        pub facetValues: Vec<FacetValue>,
     }
 
     #[derive(Deserialize, Debug, Clone)]
     pub struct FacetValue {
-        label: String,
-        value: String,
+        pub label: String,
+        pub value: String,
         #[serde(deserialize_with = "string_to_u64")]
-        count: u64,
+        pub count: u64,
     }
+
     use serde::de::{self, Deserializer};
 
     fn string_to_u64<'de, D>(deserializer: D) -> Result<u64, D::Error>
@@ -57,8 +58,6 @@ pub mod DataSet {
 
     pub async fn search(x: String) -> Result<OmicsDiResponse, Box<dyn std::error::Error>> {
         let accept_header = "application/json";
-        let x: String =
-            "TAXONOMY%3A%204787%20AND%20omics_type%3A%20Transcriptomics&start=0&size=2".into();
         let query_url = format!(
             "{}?query={}&start=2&size=4",
             REST_URL,
@@ -81,5 +80,24 @@ pub mod DataSet {
                 response.status()
             )))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::error::Error;
+
+    use DataSet::search;
+
+    use super::*;
+    #[tokio::test]
+    async fn test_input() -> Result<(), Box<dyn Error>> {
+        let x: String =
+            "TAXONOMY%3A%204787%20AND%20omics_type%3A%20Transcriptomics GSE13580&start=0&size=2"
+                .into();
+        let search_string = x;
+        let mut result = search(search_string).await?;
+        assert_eq!("E-GEOD-13580", result.datasets.pop().unwrap().id);
+        Ok(())
     }
 }
